ebfe302e2370d24db3cabce24178c4f2a257fe55
Arwinder Singh <arwindersinghh@gmail.com>
2021-11-08 02:50:59 -0500

    console something out

--- client/src/UserPage.js
+++ client/src/UserPage.js
@@ -291,12 +291,12 @@ function UserPage () {
     
     //This will sort our tasks Array for us using their starting time in milliseconds
     console.log(tasks);    
-    let sortedTasks = tasks.data ? tasks.data.sort((a, b) => {
-        const millisecondsA = new Date(a.startTime).getTime();
-        const millisecondsB = new Date(b.startTime).getTime();        
+    // let sortedTasks = tasks.data ? tasks.data.sort((a, b) => {
+    //     const millisecondsA = new Date(a.startTime).getTime();
+    //     const millisecondsB = new Date(b.startTime).getTime();        
 
-        return millisecondsA - millisecondsB
-    }).filter(task =>  (Date.parse(new Date(task.startTime)) < Date.parse(dateToCompare()) || !task.startTime)) : [];
+    //     return millisecondsA - millisecondsB
+    // }).filter(task =>  (Date.parse(new Date(task.startTime)) < Date.parse(dateToCompare()) || !task.startTime)) : [];
     
     //console.log(daysToFilter);
     //console.log(sortedTasks);
