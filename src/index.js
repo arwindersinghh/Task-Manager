11ad399b5f4d63ce4c2c795cabc1e8998951dd09
Arwinder Singh <arwindersinghh@gmail.com>
2021-11-08 02:03:52 -0500

    removing middleware

--- src/index.js
+++ src/index.js
@@ -30,21 +30,9 @@ const port = process.env.PORT;
 
 
 app.use(express.json());
-const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://arvy-task-manager.herokuapp.com/']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
-//app.use(cors());
+
+
+app.use(cors());
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
