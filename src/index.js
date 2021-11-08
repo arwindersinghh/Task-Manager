e2f4e3441dfcb4c3eccb138cf42cc64c5650753b
Arwinder Singh <arwindersinghh@gmail.com>
2021-11-08 02:33:27 -0500

    trying to delete post-build heroku script

--- src/index.js
+++ src/index.js
@@ -41,6 +41,13 @@ app.use(cors());
 //       res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 //     });
 //   }
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    })
+}
 app.use('/api/users', require('./api/users'));
 app.use('/api/tasks', require('./api/tasks'));
 
