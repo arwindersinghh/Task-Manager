94cdb8aab5c69c55b636ad150990d36753c8b725
Arwinder Singh <arwindersinghh@gmail.com>
2021-11-08 01:49:13 -0500

    for heroku

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('./db/mongoose');
 const User = require('./models/User');
 const Task = require('./models/Task');
+const path = require('path');
 
 
 
@@ -29,7 +30,29 @@ const port = process.env.PORT;
 
 
 app.use(express.json());
-app.use(cors());
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://arvy-task-manager.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+//app.use(cors());
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 app.use('/api/users', require('./api/users'));
 app.use('/api/tasks', require('./api/tasks'));
 
