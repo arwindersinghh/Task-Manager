3463fc3c345c165bcf8ee0a3ea85151d330bfeee
Arwinder Singh <arwindersinghh@gmail.com>
2021-11-08 02:07:16 -0500

    removing middleware

--- src/index.js
+++ src/index.js
@@ -35,10 +35,10 @@ app.use(express.json());
 app.use(cors());
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
   // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
   }
 app.use('/api/users', require('./api/users'));
