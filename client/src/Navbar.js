5bbe270453f9fe9001aab96b7e8e64893b5c8fd8
Arwinder Singh <arwindersinghh@gmail.com>
2021-11-13 00:03:35 -0500

    adding router

---
+++ client/src/Navbar.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react';
+import "./Navbar.css"
+import { Link, useNavigate } from 'react-router-dom';
+
+
+
+
+
+const Navbar = () => {
+
+    
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const history = useNavigate();
+
+    const token = window.localStorage.getItem('token')
+    
+    useEffect(() => {
+        setIsLoggedIn(token);
+    }, [token])
+
+    
+    
+    
+
+        
+
+        const logout = async() => {    
+            window.localStorage.removeItem('token');
+            //console.log(window.localStorage.getItem('token'))
+            setIsLoggedIn(false);
+
+            history('/home')
+          }
+
+          if(!isLoggedIn){
+              <div> You've been logged out !</div>
+          }
+
+        return(<div className="parent-container">
+            <Link to="/home"> <button> Home </button> </Link>
+            {!token ? <Link to="/login"><button> Login </button> </Link> : null }
+            {isLoggedIn ? <Link to="/me"> <button> Userpage </button></Link> :
+            null}
+            {!token ? <Link to="/signup"><button> Sign Up </button></Link> :
+            <li>{ isLoggedIn ? <button style={{ color: "red", width:"100px", height:"30px"}} onClick={logout}> Logout </button> : null }</li>}
+        </div>)
+    
+    
+}
+
+export default Navbar;
